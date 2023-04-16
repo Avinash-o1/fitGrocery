4aa86d0c9411180f8b26a34bc2c1f56c144eb68e
avinash <@avinashchandr6123@gmail.com>
2023-04-16 12:10:40 +0530

    added product slider in product page

---
+++ src/COMPONENTS/Product/ProductSlider.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import ProductCard from './ProductCard';
+import './ProductSlider.css'
+
+const ProductSlider = ({product , categoryName}) => {
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items:  7
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  return (
+    <div className='productsliderout'>
+      <h1> {categoryName} </h1>
+
+      <Carousel responsive={responsive}
+      autoPlay={true}
+      autoPlaySpeed={2000}
+      swipeable={true}
+      draggable={true}
+      infinite={true}
+      >
+       { product?.map((item)=>{
+        return (
+          <ProductCard data={item} key={item.ProductId}/>
+        )
+       })}
+      </Carousel>
+    </div>
+  )
+}
+
+export default ProductSlider
--- src/PAGES/ProductPage/ProductPage.js
+++ src/PAGES/ProductPage/ProductPage.js
@@ -5,6 +5,7 @@ import Navbar from '../../COMPONENTS/NAVBAR/Navbar';
 import Footer1 from '../../COMPONENTS/FOOTER/Footer1';
 import Footer2 from '../../COMPONENTS/FOOTER/Footer2';
 import './ProductPage.css'
+import ProductSlider from '../../COMPONENTS/Product/ProductSlider';
 
 const ProductPage = () => {
   const { prodId } = useParams();
@@ -13,6 +14,8 @@ const ProductPage = () => {
   const [activeImg, setActiveImg] = useState({})
   const [count, setCount] = useState(1)
   const [showReview, setShowReview] = useState(false)
+  const [rating, setRating] = useState(0)
+  let arr = [1, 2, 3, 4, 5]
 
   const findData = () => {
     for (let i = 0; i < data.length; i++) {
@@ -23,7 +26,7 @@ const ProductPage = () => {
     return null;
   }
 
-  const fetchProductData = async() => {
+  const fetchProductData = async () => {
     let temp = {
       'code': 200,
       'status': 'success',
@@ -38,7 +41,8 @@ const ProductPage = () => {
 
   useEffect(() => {
     fetchProductData();
-  },[] )
+    window.scrollTo(0, 0);
+  }, [])
 
 
   return (
@@ -59,17 +63,17 @@ const ProductPage = () => {
         <div className='c11'>
           <div className='imgset'>
             {
-              images?.map((item, i) =>{
-                return(
-                  <div  onClick={()=>{setActiveImg(item)}} className='imgsmall'>
-                    <img src={item.image} alt='product' className= {activeImg?.id == item.id ? 'active' : ''}/>
+              images?.map((item, i) => {
+                return (
+                  <div onClick={() => { setActiveImg(item) }} className='imgsmall'>
+                    <img src={item.image} alt='product' className={activeImg?.id == item.id ? 'active' : ''} />
                   </div>
                 )
               })
             }
           </div>
           <div className='imgbig'>
-            <img src={activeImg?.image} alt='product'/>
+            <img src={activeImg?.image} alt='product' />
           </div>
         </div>
         <div className='c12'>
@@ -80,66 +84,103 @@ const ProductPage = () => {
               <span>${productData?.ProductPrice * count}</span>
             </p>
             <div className='incrdecr'>
-              <button onClick={() => { setCount(count > 1 ? count - 1 : count ) }}>-</button>
+              <button onClick={() => { setCount(count > 1 ? count - 1 : count) }}>-</button>
               <p>{count}</p>
               <button onClick={() => { setCount(count < productData?.ProductQuantity ? count + 1 : count) }}>+</button>
             </div>
           </div>
           <div className='btncont'>
-            <button onClick={()=>{alert('added to cart')}}>Add to cart</button>
-            <button onClick={()=>{alert('Buy now is working')}}> Buy Now</button>
+            <button onClick={() => { alert('added to cart') }}>Add to cart</button>
+            <button onClick={() => { alert('Buy now is working') }}> Buy Now</button>
           </div>
         </div>
       </div>
       <div className='pc2'>
         <div className='tabs'>
-          <button className={showReview ? 'inactive' : 'active'} onClick={()=>{setShowReview(false)}}>Description</button>
-          <button className={showReview ? 'active' : 'inactive'} onClick={()=>{setShowReview(true)}} >Review</button>
+          <button className={showReview ? 'inactive' : 'active'} onClick={() => { setShowReview(false) }}>Description</button>
+          <button className={showReview ? 'active' : 'inactive'} onClick={() => { setShowReview(true) }} >Review</button>
         </div>
         {
-          !showReview ? 
-          <p className='desc'>{productData?.ProductDescription}</p> 
-          : <div className='reviewcont'>
-            <form>
-              <div className='fromgroup'>
-                <label htmlFor=''>Name</label>
-                <input type="text"></input>
-              </div>
-              <div className='fromgroup'>
-                <label htmlFor=''>Email</label>
-                <input type="email"></input>
-              </div>
-              <div className='fromgroup'>
-                <label htmlFor=''>Review</label>
-                <textarea cols="30" rows="10"></textarea>
-              </div>
-              <button>Submit</button>
-            </form>
+          !showReview ?
+            <p className='desc'>{productData?.ProductDescription}</p>
+            : <div className='reviewcont'>
+              <form>
+                <div className='fromgroup'>
+                  <label htmlFor=''>Name</label>
+                  <input type="text"></input>
+                </div>
+                <div className='fromgroup'>
+                  <label htmlFor=''>Email</label>
+                  <input type="email"></input>
+                </div>
+                <div className='fromgroup'>
+                  <label htmlFor=''>Review</label>
+                  <textarea cols="30" rows="10"></textarea>
+                </div>
+                <button>Submit</button>
+                <div className='fromgroup'>
+                  <label htmlFor=''>Rating</label>
+                  <div className='rating'>
+                    {
+                      arr.map((value) => {
+                        return (
+                          <div className='star' onClick={() => { setRating(value) }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={value <= rating ? "w-6 h-6 staractive" : "w-6 h-6 starinactive"}>
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                            </svg>
 
-            <div className='allreview'>
-            <h1 className='head1'>Product Reviews</h1>
-              {
-                productData?.ProductReviews?.map((item,i)=>{
-                  return(
-                    <div className='review'>
-                      <div className='reviewhead'>
-                        <p className='name'>{item.Name}</p>
-                        <span className='date'>{item.Date}</span>
+                          </div>
+                        )
+                      })
+                    }
+                  </div>
+                </div>
+              </form>
+
+              <div className='allreview'>
+                <h1 className='head1'>Product Reviews</h1>
+                {
+                  productData?.ProductReviews?.map((item, i) => {
+                    return (
+                      <div className='review'>
+                        <div className='reviewhead'>
+                          <p className='name'>{item.Name}</p>
+                          <div className='rating1'>
+                            {
+                              arr.map((value) => {
+                                return (
+                                  <div className='star' >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={value <= item.Rating ? "w-6 h-6 staractive" : "w-6 h-6 starinactive"}>
+                                      <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                                    </svg>
+
+                                  </div>
+                                )
+                              })
+                            }
+                          </div>
+                          <span className='date'>{item.Date}</span>
+                        </div>
+                        <div className='reviewbody'>
+                          {item.Review}
+                        </div>
                       </div>
-                      <div className='reviewbody'>
-                        {item.Review}
-                      </div>
-                    </div>
-                    
-                  )
-                })
-              }
+
+                    )
+                  })
+                }
+              </div>
             </div>
-          </div>
         }
       </div>
-      <Footer1/>
-      <Footer2/>
+      <div className='slidercont'>
+        <ProductSlider product={data} categoryName= 'Related Products' />
+      </div>
+      <div className='slidercont'>
+        <ProductSlider product={data} categoryName= 'Explore More' />
+      </div>
+      <Footer1 />
+      <Footer2 />
     </div>
   )
 }
